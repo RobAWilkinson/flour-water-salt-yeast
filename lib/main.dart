e33ccc884eae429d3b202243bbe296f45d4cffd5
Rob Wilkinson <1959628+RobAWilkinson@users.noreply.github.com>
2019-10-26 22:08:49 -0400

    Hello world stateless

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,25 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    new Center(
-      child: new Text(
-        'Hello, world!'
-      ),
-    ),
+    new MaterialApp(
+      title: 'Hello World App',
+      home: new myApp(),
+    )
   );
+}
+
+class myApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Hello World App'),
+      ),
+      body: new Center(
+        child: new Text(
+          'Hello, world!'
+        ),
+      ),
+    );
+  }
 }
