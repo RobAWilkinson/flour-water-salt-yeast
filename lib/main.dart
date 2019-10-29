f3127d598685f552e0057d87bec3db255b326e24
Rob Wilkinson <1959628+RobAWilkinson@users.noreply.github.com>
2019-10-28 23:23:54 -0400

    can display a list of recipes

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,61 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp( MaterialApp(
+  runApp(MaterialApp(
     title: 'Hello World App',
-    home:  MyApp(),
+    home: MyApp(),
   ));
 }
 
-
 class MyApp extends StatelessWidget {
+  final recipes = [
+    {
+      'name': 'White Bread',
+      'steps': [
+        {'text': 'annealease'},
+        {'text': 'knead'},
+        {'text': 'rise'},
+        {'text': 'cook'}
+      ]
+    },
+  ];
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar:  AppBar(
-        title:  Text('Amys App'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Amys App'),
       ),
-      body:  Container(
+      body: Container(
         padding: EdgeInsets.all(20.0),
-        child:  Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[ AwesomeButton(),
-
-          ],
-        ),
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: recipes.map(_recipeBuilder).toList()),
       ),
     );
+  }
+
+  Widget _recipeBuilder(Map<String, Object> recipe) {
+    final String name = recipe['name'];
+    final List<Map<String, Object>> steps = recipe['steps'];
+    return Card(
+        child: Container(
+            padding: EdgeInsets.all(20),
+            child: ListBody(children: <Widget>[
+              Text("Recipe: $name"),
+              ...steps.map(_stepBuilder).toList()
+            ])));
+  }
+
+  Widget _stepBuilder(Map<String, Object> step) {
+    final String stepText = step['text'];
+    return Text('Step: $stepText');
   }
 }
 
 class AwesomeButton extends StatefulWidget {
   @override
   AwesomeButtonState createState() {
-    return  AwesomeButtonState();
+    return AwesomeButtonState();
   }
 }
 
@@ -49,7 +73,6 @@ class AwesomeButtonState extends State<AwesomeButton> {
 
   @override
   Widget build(BuildContext context) {
-    return  RaisedButton(
-        child:  Text(displayedString), onPressed: onPressed);
+    return RaisedButton(child: Text(displayedString), onPressed: onPressed);
   }
 }
